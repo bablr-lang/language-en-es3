dccc584055defa483a6f2c380679a73143c2fde3
Conrad Buck <conartist6@gmail.com>
2024-07-07 07:47:11 -0600

    wip

---
+++ test/grammar.test.js
@@ -0,0 +1,20 @@
+import { dedent } from '@qnighy/dedent';
+// eslint-disable-next-line import/no-unresolved
+import * as language from '@bablr/language-es3';
+import { buildTag } from 'bablr';
+import { debugEnhancers } from '@bablr/helpers/enhancers';
+import { expect } from 'expect';
+import { printPrettyCSTML } from '@bablr/agast-helpers/tree';
+
+let enhancers = undefined;
+
+describe('@bablr/language-es3', () => {
+  describe('Program', () => {
+    const js = (...args) =>
+      printPrettyCSTML(buildTag(language, 'Program', undefined, enhancers)(...args));
+
+    it(';', () => {
+      expect(js`;`).toEqual(dedent`\n`);
+    });
+  });
+});
