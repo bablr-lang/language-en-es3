fe1cf6e5c2f38915cce268c50fbbf2d4679d6f03
Conrad Buck <conartist6@gmail.com>
2024-07-26 12:55:14 -0600

    language-en

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -1,6 +1,6 @@
 import { dedent } from '@qnighy/dedent';
 // eslint-disable-next-line import/no-unresolved
-import * as language from '@bablr/language-es3';
+import * as language from '@bablr/language-en-es3';
 import { buildTag } from 'bablr';
 import { debugEnhancers } from '@bablr/helpers/enhancers';
 import { expect } from 'expect';
@@ -8,7 +8,7 @@ import { printPrettyCSTML } from '@bablr/agast-helpers/tree';
 
 let enhancers = undefined;
 
-describe('@bablr/language-es3', () => {
+describe('@bablr/language-en-es3', () => {
   describe('Program', () => {
     const js = (...args) =>
       printPrettyCSTML(buildTag(language, 'Program', undefined, enhancers)(...args));
