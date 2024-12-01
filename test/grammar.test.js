06ff02acd4e663ed5a3ea208ad7d73f83d822d97
Conrad Buck <conartist6@gmail.com>
2024-12-01 09:48:57 -0700

    wip

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -1,19 +1,19 @@
+import { spam } from '@bablr/boot';
 import { dedent } from '@qnighy/dedent';
 // eslint-disable-next-line import/no-unresolved
 import * as language from '@bablr/language-en-es3';
 import { buildTag, Context, AgastContext } from 'bablr';
 import { debugEnhancers } from '@bablr/helpers/enhancers';
 import { expect } from 'expect';
-import { printPrettyCSTML } from '@bablr/agast-helpers/tree';
-
-import { buildFullyQualifiedSpamMatcher } from '@bablr/agast-vm-helpers';
+import { printPrettyCSTML } from '@bablr/helpers/tree';
+import { buildIdentifier, buildString } from '@bablr/agast-vm-helpers';
 
 let enhancers = undefined;
 
 const ctx = Context.from(AgastContext.create(), language, enhancers?.bablrProduction);
 
 const buildJSTag = (type) => {
-  const matcher = buildFullyQualifiedSpamMatcher({}, language.canonicalURL, type);
+  const matcher = spam`<$${buildString(language.canonicalURL)}:${buildIdentifier(type)} />`;
   return buildTag(ctx, matcher, undefined, { enhancers });
 };
 
@@ -21,12 +21,33 @@ const print = (tree) => {
   return printPrettyCSTML(tree, { ctx });
 };
 
-describe.skip('@bablr/language-en-es3', () => {
+describe('@bablr/language-en-es3', () => {
   describe('Program', () => {
     const js = buildJSTag('Program');
 
     it(';', () => {
-      expect(print(js`;`)).toEqual(dedent`\n`);
+      expect(print(js`;`)).toEqual(dedent`\
+        <!0:cstml bablr-language='https://bablr.org/languages/universe/es3'>
+        <$>
+          .:
+          <$Program>
+            body[]: []
+            body[]: <$EmptyStatement />
+            separators[]: []
+            separators[]: <*Punctuator ';' />
+          </>
+        </>\n`);
+    });
+
+    it('true', () => {
+      expect(print(js`true`)).toEqual(dedent`\
+        <!0:cstml bablr-language='https://bablr.org/languages/universe/es3'>
+        <$>
+          .:
+          <$Program>
+            body[]: []
+          </>
+        </>\n`);
     });
   });
 });
