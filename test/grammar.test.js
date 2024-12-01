34c8398e9ac1fe2c914bb88694d36a6d2261ae4e
Conrad Buck <conartist6@gmail.com>
2024-12-01 09:48:57 -0700

    twip

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -1,32 +1,53 @@
-// import { dedent } from '@qnighy/dedent';
-// // eslint-disable-next-line import/no-unresolved
-// import * as language from '@bablr/language-en-es3';
-// import { buildTag, Context, AgastContext } from 'bablr';
-// import { debugEnhancers } from '@bablr/helpers/enhancers';
-// import { expect } from 'expect';
-// import { printPrettyCSTML } from '@bablr/agast-helpers/tree';
+import { spam } from '@bablr/boot';
+import { dedent } from '@qnighy/dedent';
+// eslint-disable-next-line import/no-unresolved
+import * as language from '@bablr/language-en-es3';
+import { buildTag, Context } from 'bablr';
+import { debugEnhancers } from '@bablr/helpers/enhancers';
+import { expect } from 'expect';
+import { printPrettyCSTML } from '@bablr/helpers/tree';
+import { buildIdentifier, buildString } from '@bablr/helpers/builders';
 
-// import { buildFullyQualifiedSpamMatcher } from '@bablr/helpers/builders';
+let enhancers = undefined;
 
-// let enhancers = undefined;
+const ctx = Context.from(language, enhancers?.bablrProduction);
 
-// const ctx = Context.from(AgastContext.create(), language, enhancers?.bablrProduction);
+const buildJSTag = (type) => {
+  const matcher = spam`<$${buildString(language.canonicalURL)}:${buildIdentifier(type)} />`;
+  return buildTag(ctx, matcher, undefined, { enhancers });
+};
 
-// const buildJSTag = (type) => {
-//   const matcher = buildFullyQualifiedSpamMatcher({}, language.canonicalURL, type);
-//   return buildTag(ctx, matcher, undefined, { enhancers });
-// };
+const print = (tree) => {
+  return printPrettyCSTML(tree, { ctx });
+};
 
-// const print = (tree) => {
-//   return printPrettyCSTML(tree, { ctx });
-// };
+describe('@bablr/language-en-es3', () => {
+  describe('Program', () => {
+    const js = buildJSTag('Program');
 
-// describe.skip('@bablr/language-en-es3', () => {
-//   describe('Program', () => {
-//     const js = buildJSTag('Program');
+    it(';', () => {
+      expect(print(js`;`)).toEqual(dedent`\
+        <!0:cstml bablr-language='https://bablr.org/languages/universe/es3'>
+        <$>
+          .:
+          <$Program>
+            body[]: []
+            body[]: <$EmptyStatement />
+            separators[]: []
+            separators[]: <*Punctuator ';' />
+          </>
+        </>\n`);
+    });
 
-//     it(';', () => {
-//       expect(print(js`;`)).toEqual(dedent`\n`);
-//     });
-//   });
-// });
+    it('true', () => {
+      expect(print(js`true`)).toEqual(dedent`\
+        <!0:cstml bablr-language='https://bablr.org/languages/universe/es3'>
+        <$>
+          .:
+          <$Program>
+            body[]: []
+          </>
+        </>\n`);
+    });
+  });
+});
